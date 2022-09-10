5e5e4e80ae86c8d5b25b0e087b80aba70a5d10f1
aiskandar <aurelia.iskandar@student.unimelb.edu.au>
2022-09-10 12:44:10 +1000

    connected pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/LoginPage';
+import SignUp from './pages/SignupPage';
+import { Router } from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>} /> 
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </Router>
   );
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
