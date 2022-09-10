2fe2062c2809325dba231ef3f4479141e3fec572
yssoh <yongqi.soh@student.unimelb.edu.au>
2022-09-10 15:10:07 +1000

    made classlist and card

---
+++ src/componentos/CardList.js
@@ -0,0 +1,13 @@
+import Card from './Card'
+import './CardList.css';
+
+export default function CardList( { cards } ){
+
+    return (
+        <div className = "card-list">
+            {cards.map(c => (
+                <Card key={c.id} card={c} /> /* c is each individual row of cardInput*/
+                ))}  
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,14 @@ import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import Card from '../componentos/Card.js'
+import CardList from '../componentos/CardList.js'
+
+const cardInput = [
+    {id: 1, name:"joe mama", img: "joemama.jpg"},
+    {id: 2, name:"bingus bingus", img: "bingus bingus.png"}
+    // {id: 3, name:"joe mama", img: "joemama.jpg"},
+    // {id: 4, name:"joe mama", img: "joemama.jpg"},
+];
 
 export default function Home() {
     return (
@@ -12,7 +20,7 @@ export default function Home() {
                 <Grid item xs={5.3}></Grid>
                 <Grid className="homeContent" item xs={6.2}>
                     <h2>Home</h2>
-                    <Card card = {{name: "joe mama"}}/>
+                    <CardList cards={cardInput}/>
                 </Grid>
             </Grid>
         </div>
