54ebf65aa581243e1cbdf283970001e3c060c9d1
yssoh <yongqi.soh@student.unimelb.edu.au>
2022-09-11 13:59:51 +1000

    best pictures now added!

--- src/componentos/StickerCollection.js
+++ src/componentos/StickerCollection.js
@@ -2,6 +2,13 @@ import React from "react";
 import Card from "./Card";
 import "./StickerCollection.css";
 import joemama from "./joemama.jpg";
+import amogus from "./amogus.jpg";
+import birb from "./birb.jpg";
+import cat from "./cat.jpg";
+import elmo from "./elmo.jpg";
+import frug from "./frug.jpg";
+import kermit from "./kermit.jpg";
+import uhhh from "./uhhh.jpg";
 
 export default function StickerCollection(){
     return (
@@ -10,15 +17,15 @@ export default function StickerCollection(){
             <hr/>
             <div className="row">
                 <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
+                <img src={amogus} alt="amogus"/>
+                <img src={birb} alt="birb"/>
+                <img src={cat} alt="cat"/>
             </div>
             <div className="row">
-                <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
-                <img src={joemama} alt="Joe mama"/>
+                <img src={elmo} alt="elmo"/>
+                <img src={frug} alt="frug"/>
+                <img src={kermit} alt="kermit"/>
+                <img src={uhhh} alt="uhhh"/>
             </div>    
 
 
--- src/componentos/WishBanner.js
+++ src/componentos/WishBanner.js
@@ -3,16 +3,19 @@ import Card from "./Card";
 import "./WishBanner.css";
 
 import joemama from "./joemama.jpg";
+import kermit from "./kermit.jpg";
+import uhhh from "./uhhh.jpg";
+import amogus from "./amogus.jpg";
 
 export default function WishBanner(){
     return (
         <>
             <Card id = "wishbanner" title="Standard Wish Banner">
                 <div className="wishbanner-container">
+                    <img src={amogus} alt="amogus"/>
+                    <img src={uhhh} alt="uhhh"/>
                     <img src={joemama} alt="Joe mama"/>
-                    <img src={joemama} alt="Joe mama"/>
-                    <img src={joemama} alt="Joe mama"/>
-                    <img src={joemama} alt="Joe mama"/>
+                    <img src={kermit} alt="kermit"/>
                 </div>
 
                 <div className="button-container">
